e65d5886a3f1e13adf9fac6fa75a78f8aaa9d788
lorra <lgxlg3@nottingham.ac.uk>
2015-04-20 13:49:27 +0100

    Add function to create encounter histories

---
+++ code/fnEncounterHistory.R
@@ -0,0 +1,33 @@
+# function to create encounter histories (returns a list with a full encounter 
+# history (one column per session), and one for input to mark (one field with 
+# entire encounter history)). At the moment requires as input a data frame 
+# (filtered to only include one species) with the band number and the session. 
+# In the input session is the name of the session column and band.number is the 
+# name of the unique individual identifier column
+
+EncounterHistory <- function(data, session, band.number) {
+        names(data)[which(names(data)==session)]  <- "session.id"
+        names(data)[which(names(data)==band.number)]  <- "band.id"
+        
+        # will require unique identifier for each row
+        data$id <- 1:nrow(data)
+        
+        # First part creates one line per record
+        eh.full <- mutate(data, count=1) %>%
+                spread(session.id, count) 
+        
+        # drop id column
+        eh.full <- eh.full[, !names(eh.full) %in% "id"]
+        
+        # Set all NAs to zero
+        eh.full[is.na(eh.full)] <- 0
+        
+        # Final part summarises and creates one line per species/band.number combo
+        eh.full <- group_by(eh.full, band.id) %>%
+                summarise_each(funs(sum))
+        
+        # concatenate the sessions for input to mark (i.e. as e.g. 100010100)
+        eh.mark <- data.frame(select(eh.full, 1), ch = do.call(paste0, eh.full[,2:ncol(eh.full)]))
+        
+        eh <- list(eh.full = eh.full, eh.mark = eh.mark)
+}
