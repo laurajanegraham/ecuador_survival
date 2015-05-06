48cf847e8be9dda097a3a9e9edacbfc506968f12
lorra <lgxlg3@nottingham.ac.uk>
2015-05-06 17:56:40 +0100

    moved repeated parts of code out to functions

---
+++ code/fnSurvivalAnalysisMARK.R
@@ -0,0 +1,11 @@
+# This file can be edited change the models that are in the analysis. 
+SurvivalAnalysisMARK <- function(sp_proc, sp_ddl){
+        Phi.dot <- list(formula=~1)
+        Phi.habitat <- list(formula=~habitat)
+        Phi.TSM <- list(formula=~TSM)
+        Phi.TSMhabitat <- list(formula=~TSM+habitat)
+        p.dot <- list(formula=~1)
+        #p.habitat <- list(formula=~habitat)
+        cml <- create.model.list("CJS")
+        mark.wrapper(cml,data=sp_proc,ddl=sp_ddl,output=FALSE)
+}
--- code/survival_analysis_MARK.R
+++ code/survival_analysis_MARK.R
@@ -4,49 +4,13 @@ require(dplyr)
 require(tidyr)
 require(RMark)
 require(R2WinBUGS)
+
+source("code/fnCleanBandingDat.R")
 source("code/fnEncounterHistory.R")
+source("code/fnSurvivalAnalysisMARK.R")
 
 # Load and format banding data -------------------------------------------------
-
-# read in the table from full_data.mdb and write out to banding_sheet.csv - this
-# gets rid of column attribute issues
-banding.dat <- mdb.get("data/full_data.mdb", tables="Banding Sheet")
-write.csv(banding.dat, file="data/banding_sheet.csv")
-
-# re-read in data
-banding.dat <- read.csv("data/banding_sheet.csv", stringsAsFactors = FALSE)
-
-# remove records with no species, session or band
-banding.dat.clean <- filter(banding.dat, Band.Number != 99999, 
-                            !(Specie.Name %in% c("", "U", "99999"))
-                            , substr(Session, nchar(Session)-1, nchar(Session)) 
-                            %in% c("06", "07", "08", "09", "10", "11", "12")
-                            )
-
-# create a lookup for the sessions so that they are ordered correctly
-session.lookup <- data.frame(Session = unique(banding.dat.clean$Session))
-session.name.length <- nchar(as.character(session.lookup$Session))
-session.lookup$session_new <- paste0(
-        "session_"
-        , substr(session.lookup$Session, session.name.length - 1, session.name.length)
-        , "_"
-        , substr(session.lookup$Session, 1, 3))
-
-# merge lookup with the dataset 
-banding.dat.clean <- merge(banding.dat.clean, session.lookup)
-
-# Create function to run analysis in MARK --------------------------------------
-# This function can be adjusted to change the models analysed by MARK
-survival_analysis <- function(){
-        Phi.dot <- list(formula=~1)
-        Phi.habitat <- list(formula=~habitat)
-        Phi.TSM <- list(formula=~TSM)
-        Phi.TSMhabitat <- list(formula=~TSM+habitat)
-        p.dot <- list(formula=~1)
-        #p.habitat <- list(formula=~habitat)
-        cml <- create.model.list("CJS")
-        mark.wrapper(cml,data=sp_proc,ddl=sp_ddl,output=FALSE)
-}
+banding.dat.clean <- CleanBandingDat()
 
 # Analysis for individual species ----------------------------------------------
 species.list <- unique(banding.dat.clean$Specie.Name) 
@@ -57,9 +21,8 @@ res <- list()
 for(species in species.list){
         # get records for species, get necessary columns & set the habitat type
         sp_dat <- filter(banding.dat.clean, Specie.Name == species) %>%
-                select(Band.Number, session_new, Location) %>%
-                mutate(habitat = ifelse(Location %in% c("LLAV", "SANA"), "Scrub",
-                                        ifelse(Location == "MASE", "Native", "Introduced")))
+                select(Band.Number, session_new, Location)
+                
 
         # create species' encounter history
         sp_eh <- EncounterHistory(sp_dat[,c("Band.Number", "session_new")], 
@@ -91,6 +54,6 @@ for(species in species.list){
                             bins=c(0,.5,21),name="TSM")
         
         # run MARK analysis ----------------------------------------------------
-        res[[species]] <- survival_analysis()
+        res[[species]] <- SurvivalAnalysisMARK(sp_proc, sp_ddl)
 }
 
