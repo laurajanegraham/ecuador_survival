c4e8c3d275c55dcfa840a5cd415a8c8c5f284e12
lorra <lgxlg3@nottingham.ac.uk>
2015-05-07 16:31:22 +0100

    add in code for fixed group effects and random time effects (2 seperate models)

--- code/survival_analysis_winBUGS.R
+++ code/survival_analysis_winBUGS.R
@@ -1,6 +1,7 @@
 require(R2WinBUGS)
 source("code/fnCleanBandingDat.R")
 source("code/fnEncounterHistory.R")
+source("code/fnKnownStateInits.R")
 
 # Load and format banding data -------------------------------------------------
 banding.dat.clean <- CleanBandingDat()
@@ -8,15 +9,31 @@ banding.dat.clean <- CleanBandingDat()
 # initially going to be testing on the C. iris records
 species <- "COELIGENA IRIS"
 
-# Create group variable
-group <- c(rep(1, dim(CH.F)[1]), rep(2, dim(CH.M)[1]))
+# get records for species, get necessary columns
+sp_dat <- filter(banding.dat.clean, Specie.Name == species) %>%
+        select(Band.Number, session_new, habitat)
+
+sp_eh <- EncounterHistory(sp_dat[,c("Band.Number", "session_new")], 
+                          "session_new", 
+                          "Band.Number")
+
+sp_bugs <- sp_eh$eh.full
+
+sp_habitat <- select(sp_dat, Band.Number, habitat) %>%
+        unique() %>%
+        merge(sp_bugs) %>%
+        group_by(Band.Number) %>%
+        summarise_each("first")
+
+# Create group variable as a number
+group <- as.numeric(as.factor(sp_habitat$habitat))
+CH <- as.matrix(select(sp_habitat, -Band.Number, -habitat))
 
 # Create vector with occasion of marking
 get.first <- function(x) min(which(x!=0))
 f <- apply(CH, 1, get.first)
 
-# Specify model in BUGS language
-
+# Analysis of survival by habitat type -----------------------------------------
 
 # Bundle data
 bugs.data <- list(y = CH, f = f, nind = dim(CH)[1], n.occasions = dim(CH)[2], z = known.state.cjs(CH), g = length(unique(group)), group = group)
@@ -28,13 +45,39 @@ inits <- function(){list(z = cjs.init.z(CH, f), phi.g = runif(length(unique(grou
 parameters <- c("phi.g", "p.g")
 
 # MCMC settings
-ni <- 5000
+ni <- 10000
 nt <- 3
 nb <- 2000
 nc <- 3
 
-# Call WinBUGS from R (BRT 2 min)
-cjs.group <- bugs(bugs.data, inits, parameters, "cjs-group.bug", n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb, debug = TRUE, working.directory='~/.wine/drive_c/temp/Rtmp/', clearWD=TRUE)
+# Call WinBUGS from R
+cjs.group <- bugs(bugs.data, inits, parameters, "cjs-group.bug", n.chains = nc, 
+                  n.thin = nt, n.iter = ni, n.burnin = nb, debug = TRUE, 
+                  working.directory='~/.wine/drive_c/temp/Rtmp/', clearWD=TRUE)
 
 # Summarize posteriors
 print(cjs.group, digits = 3)
+
+# Analysis of survival across time (time as random factor) ---------------------
+# bundle data
+bugs.data <- list(y= CH, f = f, nind = dim(CH)[1], n.occasions = dim(CH)[2], 
+                  z = known.state.cjs(CH))
+
+# initial values
+inits <- function(){list(z = cjs.init.z(CH, f), mean.phi = runif(1, 0, 1), 
+                         sigma = runif(1, 0, 10), mean.p = runif(1, 0, 1))}
+
+# parameters monitored
+parameters <- c("mean.phi", "mean.p", "sigma2")
+
+# MCMC settings
+ni <- 10000
+nt <- 6
+nb <- 5000
+nc <- 3
+
+cjs.ran <- bugs(bugs.data, inits, parameters, "cjs-time-raneff.bug", n.chains = nc, 
+                n.thin = nt, n.iter = ni, n.burnin = nb, debug = TRUE, 
+                working.directory='~/.wine/drive_c/temp/Rtmp/', clearWD=TRUE)
+
+print(cjs.ran, digits = 3)
