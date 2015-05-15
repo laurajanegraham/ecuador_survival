1b6f5357e30070dd7ddecee6a419234e3b8a61eb
lorra <lgxlg3@nottingham.ac.uk>
2015-05-15 08:42:58 +0100

    include code to create m-array

--- code/fnEncounterHistory.R
+++ code/fnEncounterHistory.R
@@ -37,9 +37,33 @@ EncounterHistory <- function(data, session, band.number) {
                               ch = do.call(paste0, eh.full[,2:ncol(eh.full)]),
                               stringsAsFactors = FALSE)
         
+        
+        # create m-array for input to WinBUGS 
+        # TODO: add in a bit to do by group for habitat analysis
+        CH <- eh.full[-1]
+        nind <- nrow(CH)
+        n.occasions <- ncol(CH)
+        m.array <- matrix(data = 0, ncol = n.occasions + 1, nrow = n.occasions)
+        m.array[, 1] <- colSums(CH)
+        for (i in 1:nind){
+                pos <- which(CH[i,]!=0)
+                g <- length(pos)
+                for (z in 1:(g - 1)) {
+                        m.array[pos[z], pos[z + 1]] <- m.array[pos[z], pos[z + 1]] + 1
+                }
+        }
+        
+        for (t in 1:n.occasions) {
+                m.array[t, n.occasions + 1] <- m.array[t, 1] - sum(m.array[t, 2:n.occasions])
+        }
+        
+        m.array <- m.array[1:(n.occasions - 1), 2:(n.occasions + 1)]
+        
+        
         # rename band.number column back to what it should be
         names(eh.mark)[1] <- band.number
         names(eh.full)[1] <- band.number
         
-        eh <- list(eh.full = eh.full, eh.mark = eh.mark)
+        eh <- list(eh.full = eh.full, eh.mark = eh.mark, m.array = m.array)
+        return(eh)
 }
