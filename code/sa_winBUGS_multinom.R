a930f8c8d8f51801dfaba47cb705dae29abe3354
lorra <lgxlg3@nottingham.ac.uk>
2015-05-19 11:02:14 +0100

    put code within loop for species

--- code/sa_winBUGS_multinom.R
+++ code/sa_winBUGS_multinom.R
@@ -7,26 +7,53 @@ source("code/fnKnownStateInits.R")
 # Load and format banding data -------------------------------------------------
 banding.dat.clean <- CleanBandingDat()
 
-sp_dat <- filter(banding.dat.clean, Specie.Name == species) %>%
-        select(Band.Number, session_new, habitat)
+# this is the log file that will record how long each model run took. 
+logfile.name <- paste0("logs/winbugs_log_", Sys.time(), ".txt")
+file.create(logfile.name)
 
-sp_eh <- EncounterHistory(sp_dat, "session_new", "Band.Number", "habitat")
+# get a count of how many individual captures exist for each species and then 
+# use those species with more than 100 for the initial analyses (there are 10 of
+# these species)
+species.list <- group_by(banding.dat.clean, Specie.Name) %>%
+        summarise(count = length(Specie.Name)) %>%
+        arrange(-count)
 
-marr <- sp_eh$m.array
-marr.gp <- sp_eh$m.array.gp
+species.list <- species.list[1:5,1]
+for (species in species.list$Specie.Name){
 
-bugs.data <- list(marr = marr, n.occasions = ncol(marr))
+        sp_dat <- filter(banding.dat.clean, Specie.Name == species) %>%
+                select(Band.Number, session_new, habitat)
+        
+        sp_eh <- EncounterHistory(sp_dat, "session_new", "Band.Number", "habitat")
+        
+        marr <- sp_eh$m.array
+        marr.gp <- sp_eh$m.array.gp
+        
+        
+        # Code for the random time effects analysis ------------------------------------
+        bugs.data <- list(marr = marr, n.occasions = ncol(marr))
+        
+        inits <- function(){list(mean.phi = runif(1, 0, 1), sigma = runif(1, 0, 5), 
+                                 mean.p = runif(1, 0, 1))}  
+        
+        # Parameters monitored
+        parameters <- c("phi", "mean.p", "mean.phi", "sigma2", "sigma2.real", "fit", "fit.new")
+        
+        ni <- 10000
+        nt <- 3
+        nb <- 2000
+        nc <- 3
+        
+        write(paste("Analysis by habitat for", species, sep=" "), 
+              logfile.name, append = TRUE)
+        write(paste0("ni = ", ni, ", nt = ", nt, " , nb = ", nb, ", nc = ", nc), 
+              logfile.name, append = TRUE)
+        strt <- Sys.time()       
+        cjs <- bugs(bugs.data, inits, parameters, "cjs-mnl-ran-time.bug", n.chains = nc, n.thin = nt, 
+                    n.iter = ni, n.burnin = nb, debug = TRUE, 
+                    working.directory='~/.wine/drive_c/temp/Rtmp/', clearWD=TRUE)
+        
+        write(paste("Model run took", round(Sys.time()-strt, 2),  units(Sys.time()-strt), 
+                    sep = " "), logfile.name, append = TRUE)
+}
 
-inits <- function(){list(mean.phi = runif(1, 0, 1), sigma = runif(1, 0, 5), mean.p = runif(1, 0, 1))}  
-
-# Parameters monitored
-parameters <- c("phi", "mean.p", "mean.phi", "sigma2", "sigma2.real", "fit", "fit.new")
-
-ni <- 5000
-nt <- 3
-nb <- 1000
-nc <- 3
-
-cjs <- bugs(bugs.data, inits, parameters, "cjs-mnl-ran-time.bug", n.chains = nc, n.thin = nt, 
-            n.iter = ni, n.burnin = nb, debug = TRUE, 
-            working.directory='~/.wine/drive_c/temp/Rtmp/', clearWD=TRUE)
