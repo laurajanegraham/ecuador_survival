a50a83d9947feb083d91ec4b0a2c0f2441c03089
lorra <lgxlg3@nottingham.ac.uk>
2015-05-22 20:59:05 +0100

    make results analysis more generalised

--- code/analyse_results.R
+++ code/analyse_results.R
@@ -1,9 +1,13 @@
+# load packages
+require(ggplot2)
+require(R2WinBUGS)
+require(dplyr)
+
+# load results for each model
 load("results/cjs.group.habitat.rda")
 load("results/cjs.mnl.time.ran.rda")
 
-
-cjs.group.habitat[[1]]$sims.list$phi.g
-cjs.group.habitat[[1]]$mean$phi.g
+# DIC is not helpful with hierarchical models...
 group.dic <- data.frame(sapply(cjs.group.habitat, function(x) round(x$DIC, 2)))
 group.dic$Species <- rownames(group.dic)
 time.dic <- data.frame(sapply(cjs.mnl.time.ran, function(x) round(x$DIC, 2)))
@@ -12,6 +16,7 @@ dic.values <- merge(time.dic, group.dic)
 names(dic.values) <- c("Species", "time", "habitat")
 save(dic.values, file = "results/dicvalues.rda")
 
+# overall survival probabilities for the random time effects
 # with sd
 survival.res <- sapply(cjs.mnl.time.ran, function(x) {
         c(mean.phi = round(x$mean$mean.phi, 2), 
@@ -34,15 +39,26 @@ survival.res <- sapply(cjs.mnl.time.ran, function(x) {
 survival.res <- t(survival.res)
 save(survival.res, file="results/survival.res")
 
-# for one species (DIGLOSSOPIS CYANEA - best fitting model by DIC) do some model fitting
-species <- "METALLURA TYRIANTHINA"   
+# get the fit stats out of the BUGS objects and into a dataframe with species names
+fit.stats <- lapply(cjs.mnl.time.ran, function(x){
+    data.frame(fit=x$sims.list$fit, 
+               fit.new=x$sims.list$fit.new)
+})
 
-bugs.res <- cjs.mnl.time.ran[[species]]
+fit.stats <- do.call("rbind", fit.stats)
+sp <- rownames(fit.stats)
+sp <- gsub("[[:digit:]]", "", sp)
+sp <- gsub("[.]", "", sp)
+fit.stats$species <- sp
 
-plot(bugs.res$sims.list$fit, bugs.res$sims.list$fit.new, xlab = "Discrepancy actual data", 
-     ylab = "Discrepancy simulated data", las = 1)
-abline(0, 1, lwd = 2)
+# plot fit against fit.new with the 1:1 line
+ggplot(fit.stats, aes(x = fit, y = fit.new)) + 
+    geom_point(shape = 1) + 
+    geom_abline(intercept = 0, slope = 1) + 
+    facet_wrap(~species)
 
+p.vals <- group_by(fit.stats, species) %>%
+    summarise(p.val = mean(fit.new > fit))
 mean(bugs.res$sims.list$fit.new > bugs.res$sims.list$fit)
 
 # for same species look at the posterior distributions for each individual year
@@ -54,3 +70,6 @@ phi.post <- gather(phi.post, "time", "posterior", -id)
 require(ggplot2)
 ggplot(phi.post, aes(x = posterior)) + geom_density() + facet_wrap(~time) + geom_hline(yintercept = 1)
 
+sigma <- sqrt(bugs.res$sims.list$sigma2)
+hist(sigma, freq = FALSE, main = "Prior = Uniform(0, 10)", xlab=expression(sigma))
+abline(h = 0.1, lty = 2, lwd = 2)
