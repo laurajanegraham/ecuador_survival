ba50a6bbb049754c60aa8ba83c5cece58a0e4f67
lorra <lgxlg3@nottingham.ac.uk>
2015-12-01 17:15:51 -0500

    add in code to run MCMC chains in parallel

---
+++ code/BUGSParallel.R
@@ -0,0 +1,42 @@
+# BUGSParallel.R ---------------------------------------------------------------
+# Laura Graham, adapted from Heather Lynch
+
+# Allows for MCMC chains to be run in parallel
+
+
+BUGSParallelized = function(Index, bugs.seed.vec, inits, model.file, data, working.directory=NULL, params, n.thin, n.iter, n.burnin, DIC){
+    RNGset = c("Mersenne-Twister","Marsaglia-Multicarry","Super-Duper","Knuth-TAOCP-2002","Knuth-TAOCP","Wichmann-Hill","L'Ecuyer-CMRG")
+    set.seed(1,kind=RNGset[Index])
+    random.seed = runif(1,1,1e6)
+    mod = bugs(inits=inits, n.chains=1, model.file=model.file, working.directory=working.directory, data=data, parameters.to.save=params, n.thin=n.thin, n.iter=n.iter, n.burnin=n.burnin, DIC=DIC, clearWD = TRUE)
+    return(mod)
+}
+
+BUGSParallel = function(n.cores,data,inits,params,model.file,debug,n.chains,n.iter,n.burnin,n.thin,DIC=FALSE,working.directory=NULL){
+    # Start snowfall
+    sfInit(parallel=TRUE, cpus=n.cores)
+    sfLibrary(R2WinBUGS)
+    sfLibrary(snowfall)
+    sfExportAll()
+    sfClusterCall( runif, 4 )
+    bugs.seed.vec = ceiling(runif(n.chains,1,1e6))
+    # Run BUGS
+    BUGSList <- sfLapply(1:n.chains, BUGSParallelized, bugs.seed.vec =bugs.seed.vec, data=data, inits=inits, params=params,
+                         model.file= model.file, n.iter=n.iter, n.burnin=n.burnin, n.thin=n.thin, DIC=DIC, working.directory=working.directory)
+# End snowfall
+    sfStop()
+    result <- NULL
+    model <- NULL
+    for (ch in 1:n.chains) {
+        result <- abind(result, BUGSList[[ch]]$sims.array, along = 2)
+        model[[ch]] <- BUGSList[[ch]]$model
+    }
+    result <- as.bugs.array(result, model.file = model.file,program = "bugs", DIC = DIC, n.iter = n.iter, n.burnin = n.burnin,n.thin = n.thin)
+    out <- list(model = model, BUGSoutput = result, parameters.to.save = params, model.file = model.file, n.iter = n.iter, DIC = DIC)
+    class(out) <- c("rbugs.parallel", "rbugs")
+    return(out)
+}
+
+system.time(m<-JAGSParallel(3,data=Dat, inits=InitStage, params=ParsStage, model.file= "Adelie simplified life cycle v10.jags", n.chains=nc, n.iter=ni, n.burnin=nb, n.thin=nt))
+jagsfit.mcmc <- as.mcmc(m[[2]])  #m15[[2]] is the JAGSoutput
+save(jagsfit.mcmc,file="August10ModelRun1.Rdata")
